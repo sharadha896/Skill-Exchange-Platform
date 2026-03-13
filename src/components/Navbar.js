bd62baa8283b44da79a8cfa64025e4ebad582035
sharadha896 <sharadhasharu054@gmail.com>
2026-03-13 10:47:23 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,6 @@ function Navbar() {
       <div className="nav-links">
         <Link to="/">Home</Link>
         <Link to="/skills">Skills</Link>
-        <Link to="/addskill">Add Skill</Link>
         <Link to="/dashboard">Dashboard</Link>
         <Link to="/register"><button>Register</button></Link>
          <Link to="/login"><button>Login</button></Link>
